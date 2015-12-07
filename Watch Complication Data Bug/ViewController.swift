e3d1460c67cc448b64bd1ecba4cdc448c1d329b1
Stephen Anthony <steve@darjeelingapps.com>
2015-12-07 12:08:11 +0000

    Initial Commit

---
+++ Watch Complication Data Bug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Watch Complication Data Bug
+//
+//  Created by Stephen Anthony on 07/12/2015.
+//  Copyright © 2015 Darjeeling Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
